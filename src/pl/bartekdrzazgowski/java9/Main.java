10dddda5e69d24279c987d5026381224aade24a3
drzazga <bartek4674@gmail.com>
2017-09-26 22:15:15 +0200

    next features

--- src/pl/bartekdrzazgowski/java9/Main.java
+++ src/pl/bartekdrzazgowski/java9/Main.java
@@ -2,22 +2,65 @@ package pl.bartekdrzazgowski.java9;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Stream.of(1, 4, 5, 2, 3, 6, 7).dropWhile(x -> x < 5).forEach(x -> System.out.println(x));
-		
+		streams();
+		separator();
+		collections();
+		separator();
+		optionals();
+		separator();
+		processes();
+	}
+
+	private static void streams() {
+		LongStream.range(0, 10).takeWhile(i -> i < 5L).forEach(x -> System.out.print(x + " "));
+		newLine();
+		IntStream.range(10, 100).dropWhile(x -> x < 90).forEach(x -> System.out.print(x + " "));
+		newLine();
+		IntStream.iterate(10, x -> x > 0, x -> x - 1).forEach(x -> System.out.print(x + " "));
+		newLine();
+	}
+
+	private static void collections() {
 		List<Integer> list = List.of(1, 2);
 		System.out.println("List class: " + list.getClass());
 		System.out.println("List size: " + list.size());
 		
+		Set<Integer> set = Set.of(1, 2);
+		System.out.println("Set class: " + set.getClass());
+		System.out.println("Set size: " + set.size());
+		
 		Map<Integer, String> map = Map.of(1, "Gdansk", 2, "Sopot", 3, "Gdynia");
 		map.forEach((k, v) -> System.out.println(k + " => " + v));
-		
+	}
+	
+	private static void optionals() {
+		Optional.empty().or(() -> Optional.of(42)).ifPresent(x -> System.out.println("or - " + x));
+		System.out.println(Optional.of(ThreadLocalRandom.current().nextInt()).stream());
+		Optional.empty().ifPresentOrElse(x -> System.out.print("ifPresentOrElse - " + x), () -> System.out.println("ifPresentOrElse - no value"));
+	}
+	
+	private static void processes() {
 		ProcessHandle currentProcess = ProcessHandle.current();
-		System.out.println("Current Process Id: = " + currentProcess.pid());
+		System.out.println("Current Process pid: " + currentProcess.pid());
 		System.out.println("Current Process is alive: " + currentProcess.isAlive());
+		System.out.println("Current Process command: " + currentProcess.info().command());
+		System.out.println("Current Process user: " + currentProcess.info().user());
+	}
+	
+	private static void separator() {
+		System.out.println("\n---------------\n");
+	}
+	
+	private static void newLine() {
+		System.out.println("");
 	}
 }
